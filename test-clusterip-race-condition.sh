5f9ad61abec4712ae98e75993f78989928d24ee0
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-02 07:27:25 +0000

    autocommit: saved test-clusterip-race-condition.sh

--- test-clusterip-race-condition.sh
+++ test-clusterip-race-condition.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-SERVICE=test-svc-$RANDOM
+SERVICE=test-svc-$$
 kubectl create service clusterip $SERVICE --tcp=80
 IPADDR=$(kubectl get svc $SERVICE -o template='{{.spec.clusterIP}}')
 curl -m3 $IPADDR

e736e1371b138900e2ec9d883e89b9cf940109ea
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-07 11:06:00 +0000

    autocommit: saved deploy.sh

--- m3-generic/deploy.sh
+++ m3-generic/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-helm upgrade --install hasher ./generic
+helm upgrade --install hasher ./generic --set image.repository=dockercoins/hasher
 helm upgrade --install redis ./generic
 helm upgrade --install rng ./generic
 helm upgrade --install webui ./generic

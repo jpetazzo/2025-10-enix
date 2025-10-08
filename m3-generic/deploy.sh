4c39296e90b4472ef475d8d3268c2b45af61579a
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-08 08:03:07 +0000

    autocommit: saved deploy.sh

--- m3-generic/deploy.sh
+++ m3-generic/deploy.sh
@@ -3,6 +3,6 @@ set -e
 
 for COMPONENT in hasher redis rng webui worker;
 do
-  helm upgrade --install $COMPONENT --values value-$COMPONENT.yaml
+  helm upgrade --install $COMPONENT ./generic --values value-$COMPONENT.yaml
 done
 

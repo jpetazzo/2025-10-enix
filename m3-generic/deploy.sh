a875fe6dac27758b3c0fe8a44b1dbefa722afcd7
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-08 07:19:14 +0000

    autocommit: saved deploy.sh

--- m3-generic/deploy.sh
+++ m3-generic/deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-helm upgrade --install hasher ./generic --set image.repository=dockercoins/hasher
-helm upgrade --install redis ./generic
-helm upgrade --install rng ./generic
-helm upgrade --install webui ./generic
-helm upgrade --install worker ./generic
+helm upgrade --install hasher ./generic \
+  --set image.repository=dockercoins/hasher \
+  --set image.tag=v0.1 \
+  #
+helm upgrade --install redis ./generic \
+  --set image.repository=redis
+  --set image.tag=latest \
+  #
+helm upgrade --install rng ./generic \
+  --set image.repository=dockercoins/rng \
+  --set image.tag=v0.1 \
+  #
+helm upgrade --install webui ./generic \
+  --set image.repository=dockercoins/webui \
+  --set image.tag=v0.1 \
+  #
+helm upgrade --install worker ./generic \
+  --set image.repository=dockercoins/worker \
+  --set image.tag=v0.1 \
+  #

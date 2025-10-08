e7539ec8c78a83f8c26b19b026189fde68b491f7
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-08 08:02:52 +0000

    autocommit: saved deploy.sh

--- m3-generic/deploy.sh
+++ m3-generic/deploy.sh
@@ -1,25 +1,8 @@
 #!/bin/sh
 set -e
 
-helm upgrade --install hasher ./generic \
-  --set image.repository=dockercoins/hasher \
-  #
-helm upgrade --install redis ./generic \
-  --set image.repository=redis \
-  --set image.tag=latest \
-  --set readinessProbe=null \
-  --set livenessProbe=null \
-  --set service.port=6379 \
-  #
-helm upgrade --install rng ./generic \
-  --set image.repository=dockercoins/rng \
-  #
-helm upgrade --install webui ./generic \
-  --set image.repository=dockercoins/webui \
-  --set service.type=NodePort \
-  #
-helm upgrade --install worker ./generic \
-  --set image.repository=dockercoins/worker \
-  --set readinessProbe=null \
-  --set livenessProbe=null \
-  #
+for COMPONENT in hasher redis rng webui worker;
+do
+  helm upgrade --install $COMPONENT --values value-$COMPONENT.yaml
+done
+

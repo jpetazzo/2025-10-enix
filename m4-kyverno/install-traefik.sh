364b4895c1c051e084a38345b6b4de32ea74e0e8
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-16 07:25:27 +0000

    autocommit: saved install-traefik.sh

--- m4-kyverno/install-traefik.sh
+++ m4-kyverno/install-traefik.sh
@@ -8,5 +8,5 @@ helm upgrade --install --repo https://traefik.github.io/charts \
   --set tolerations[0].effect=NoSchedule \
   --set tolerations[0].key=node-role.kubernetes.io/control-plane \
   --set ports.web.hostPort=80 \
-  --set ports.websecure.hostPort \
+  --set ports.websecure.hostPort=443 \
   #

3e405a19c1a76a91ebbb7716869941c078e6f425
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-16 07:19:06 +0000

    autocommit: saved install-traefik.sh

--- m4-kyverno/install-traefik.sh
+++ m4-kyverno/install-traefik.sh
@@ -3,5 +3,7 @@
 helm upgrade --install --repo https://traefik.github.io/charts \
   traefik traefik --namespace traefik --create-namespace \
   --version 37.1.2 \
+  --set deployment.kind=DaemonSet \
+  --set service.type=ClusterIP \
   #
 

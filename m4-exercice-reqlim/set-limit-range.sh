5dc9ce051a534c52628e0885979e939663b31b43
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-10-17 07:12:07 +0000

    autocommit: saved set-limit-range.sh

--- m4-exercice-reqlim/set-limit-range.sh
+++ m4-exercice-reqlim/set-limit-range.sh
@@ -3,4 +3,5 @@ for NAMESPACE in $(
   kubectl get namespaces --selector '!name , kubernetes.io/metadata.name!=kube-system' -o name | cut -d/ -f2
   ); do
   kubectl apply -f limitrange.yaml --namespace $NAMESPACE
+  kubectl rollout restart deployment --namespace $NAMESPACE
 done
